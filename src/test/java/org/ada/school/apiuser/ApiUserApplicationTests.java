3204b7b1893d5c26e9d8bf533a5fcc15480bf805
Jose1102 <jose.castroortega1102@gmail.com>
2021-08-28 21:59:58 -0500

    ADD repository

---
+++ src/test/java/org/ada/school/apiuser/ApiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ada.school.apiuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
